335755966cdc096f1e93aa3fba043f773fa64d43
Maricel Dragan <mariaceliadragan@gmail.com>
2025-01-25 20:20:09 +0000

    add component

--- src/components/ScotlandData.tsx
+++ src/components/ScotlandData.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContainer,
+  CardFirstHeader,
+  CardSecondHeader,
+  CardThirdHeader,
+} from "../Card.styled";
+import { useDaysRequest } from "./useDaysRequest";
 
 export const ScotlandData = () => {
+  let { data: returnValues, error, isLoading } = useDaysRequest();
+  const [segmentsValue, setsegmentsValue] = useState("Scotland");
+
+  if (isLoading) return <p> Is Loading</p>;
+
+  if (error) {
+    return <p>An error has occurred!</p>;
+  }
+
   return (
-    <div>
-      <p>Scotland Bank Holidays</p>
-    </div>
+    <section>
+      {returnValues &&
+        // eslint-disable-next-line array-callback-return
+        returnValues["Scotland"].events.map((item: any) => {
+          let todaysDate = new Date().toISOString().split("T")[0];
+
+          if (item.date > todaysDate)
+            return (
+              <CardContainer>
+                <Card key={item.id}>
+                  <CardFirstHeader>Next Bank Holiday: </CardFirstHeader>
+                  <CardSecondHeader>{item.title}</CardSecondHeader>
+                  <CardThirdHeader>{item.date} </CardThirdHeader>
+                </Card>
+              </CardContainer>
+            );
+        })}
+    </section>
   );
 };
