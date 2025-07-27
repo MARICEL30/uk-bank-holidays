10f9aba6773b2e3df2bc4058f8f7af333678ebbb
Maricel Dragan <mariaceliadragan@gmail.com>
2025-07-27 20:14:45 +0100

    update parent component

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import { useDaysRequest } from "./api";
 
 const App = () => {
   let { error, isLoading } = useDaysRequest();
-
-  const [value, setValue] = useState("");
-
+  let [value, setValue] = useState("england-and-wales");
   if (isLoading) return <p> Is Loading</p>;
 
   if (error) {
@@ -20,7 +18,7 @@ const App = () => {
       <ButtonsContainer
         name=""
         segments={segments}
-        onClick={(value) => setValue(value)}
+        onClick={() => setValue(value)}
       />
     </div>
   );
